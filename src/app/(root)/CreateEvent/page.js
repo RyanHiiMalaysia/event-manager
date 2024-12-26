aa114625674e8664ff3def427d939c8788bdcc4e
Nah Xin Wen <xinwen1114@example.com>
2024-12-26 16:47:47 +0800

    Updated event creator

--- src/app/(root)/CreateEvent/page.js
+++ src/app/(root)/CreateEvent/page.js
@@ -4,101 +4,101 @@ import { useState } from 'react';
 
 const CreateEvent = () => {
   const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
   const [startdate, setStartDate] = useState('');
   const [enddate, setEndDate] = useState('');
-  const [validitydate, setValidDate] = useState('');
-  const [validitytime, setValidTime] = useState('');
+  const [duration, setDuration] = useState('');
+  const [minutes, setMinutes] = useState('');
+  const [hours, setHours] = useState('');
+  const [days, setDays] = useState('');
+  const [location, setLocation] = useState('');
+  const [maxParticipants, setmaxParticipants] = useState('');
+  const [operatingStart, setOperatingStart] = useState('');
+  const [operatingEnd, setOperatingEnd] = useState('');
 
-  const handleCreateEvent = async () => {
-    if (!title || !description || !startdate || !enddate || !validitydate || !validitytime) {
-      alert('Please fill out all fields.');
+  function Division({type, title, variable, setFunction}){
+    return ( <div className="mb-4">
+              <label className="block text-sm font-medium mb-1">{title}</label>
+              <input
+                type={type}
+                value={variable}
+                onChange={(e) => setFunction(e.target.value)}
+                className="input text-black w-full p-1 max-w-lg mx-auto border border-black rounded-lg"
+              />
+            </div>)
+   }
+
+  const handleCreateEvent = async (event) => {
+    event.preventDefault();
+
+    if(!days || !hours || !minutes){
+      alert('Please fill duration');
       return;
     }
+
+    else if (!title || !startdate || !enddate || !location || !operatingStart || !operatingEnd || !maxParticipants) {
+        alert('Please fill out all fields.');
+        return;
+      }
+
     else if (new Date(startdate) > new Date(enddate)) {
       alert('Start Date cannot be after End Date.');
       return;
     }
-    else if (new Date(validitydate) > new Date(startdate)) {
-      alert('Valid until Date cannot be after Start Date.');
-      return;
-    }
     
+    else if (new Date(operatingStart) > new Date(operatingEnd)) {
+        alert('Operating start tume cannot be after end time.');
+        return;
+      }
+
+    setDuration(`${String(days).padStart(2, "0")}:${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`);
+
     const response = await fetch('/api/events', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title, description, startdate, enddate, validitydate, validitytime }),
+      body: JSON.stringify({title , duration , startdate , enddate ,  maxParticipants, location ,  operatingStart ,  operatingEnd}),
     });
+    
 
     if (response.ok) {
       alert('Event created successfully!');
       setTitle('');
-      setDescription('');
+      setDuration('');
       setStartDate('');
       setEndDate('');
-      setValidDate('');
-      setValidTime('');
+      setLocation('');
+      setmaxParticipants('');
+      setOperatingStart('');
+      setOperatingEnd('');
     } else {
       alert('Failed to create event.');
     }
   };
 
+
   return (
+    <form onSubmit={handleCreateEvent}>
     <div className="p-4 max-w-sm mx-auto border border-gray-300 rounded-lg bg-white shadow-lg">
     <div className="p-6 max-w-lg mx-auto border border-black rounded-lg bg-transparent">
       <h1 className="text-2xl font-bold mb-4">Create Event</h1>
-      <div className="mb-4">
-        <label className="block text-sm font-medium mb-1">Event Title</label>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="input text-black w-full p-1 max-w-lg mx-auto border border-black rounded-lg"
 
-        />
-      </div>
+      <Division type="text" title="Event Title" variable={title} setFunction={setTitle} />
+
       <div className="mb-4">
-        <label className="block text-sm font-medium mb-1">Description</label>
-        <textarea
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          className="input text-black w-full p-1 max-w-lg mx-auto border border-black rounded-lg"
-        />
+        <label className="block text-sm font-medium mb-1">Duration</label>
+        <Division type="number" title="Days" variable={days} setFunction={setDays} />
+        <Division type="number" title="Hours" variable={hours} setFunction={setHours} />
+        <Division type="number"title="Minutes" variable={minutes} setFunction={setMinutes} />
       </div>
-      <div className="mb-4">
-        <label className="block text-sm font-medium mb-1">Start Date</label>
-        <input
-          type="date"
-          value={startdate}
-          onChange={(e) => setStartDate(e.target.value)}
-          className="input text-black w-full p-1 max-w-lg mx-auto border border-black rounded-lg"
-        />
-      </div>
-      <div className="mb-4">
-        <label className="block text-sm font-medium mb-1">End Date</label>
-        <input
-          type="date"
-          value={enddate}
-          onChange={(e) => setEndDate(e.target.value)}
-          className="input text-black w-full p-1 max-w-lg mx-auto border border-black rounded-lg"
-        />
-      </div>
-      <div className="mb-4">
-        <label className="block text-sm font-medium mb-1">Valid until Date</label>
-        <input
-          type="date"
-          value={validitydate}
-          onChange={(e) => setValidDate(e.target.value)}
-          className="input text-black w-full p-1 max-w-lg mx-auto border border-black rounded-lg"
-        />
-        <label className="block text-sm font-medium mb-1">Valid until Time</label>
-        <input
-          type="time"
-          value={validitytime}
-          onChange={(e) => setValidTime(e.target.value)}
-          className="input text-black w-full p-1 max-w-lg mx-auto border border-black rounded-lg"
-        />
-      </div>
+
+      <Division type="textarea" title="Location" variable={location} setFunction={setLocation} />
+      <Division type="number" title="Max Participants" variable={maxParticipants} setmaxParticipants={setLocation} />
+
+      <Division type="date" title="Start Date" variable={startdate} setFunction={setStartDate} />
+      <Division type="date" title="End Date" variable={enddate} setFunction={setEndDate} />
+  
+      <Division type="datetime-local" title="Operating Start Date Time" variable={operatingStart} setFunction={setOperatingStart} />
+      <Division type="datetime-local" title="Operating End Date Time" variable={operatingEnd} setFunction={setOperatingEnd} />
+
       <nav className='flex justify-between items-center'>
         <Link 
         href="/event"
@@ -107,7 +107,6 @@ const CreateEvent = () => {
               <span>Back</span>
           </Link>
         <button
-          onClick={handleCreateEvent}
           className="px-4 py-2 bg-green-500 text-white rounded"
         >
           Create Event
@@ -115,6 +114,7 @@ const CreateEvent = () => {
       </nav>
     </div>
     </div>
+    </form>
   );
 };
 
