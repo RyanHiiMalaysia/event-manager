a054c7da806035daac27b10ba0f1a3bc66eac6ac
Jona0002 <jona0002@student.monash.edu>
2024-12-26 16:34:12 +0800

    Adds Calendar views

---
+++ src/components/Calendar.js
@@ -0,0 +1,55 @@
+import React, { useMemo } from "react";
+import { Calendar, momentLocalizer, Views } from "react-big-calendar";
+import moment from "moment";
+import "../../styles/calendar.css";
+import WeekView from "./WeekView";
+
+const localizer = momentLocalizer(moment);
+
+export function EventCalendar({ events, onSelectEvent }) {
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 600 }}
+        onSelectEvent={onSelectEvent}
+        popup
+      />
+    </div>
+  );
+}
+
+export function ScheduleCalendar({ onSelectEvent, eventRange, freeTimes }) {
+  const { defaultDate, views } = useMemo(
+    () => ({
+      defaultDate: eventRange.start,
+      views: {
+        month: true,
+        week: WeekView,
+      },
+    }),
+    []
+  );
+
+  return (
+    <div className="height-400">
+      <Calendar
+      localizer={localizer}
+      events={freeTimes}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: '100%' }}
+      defaultView={Views.WEEK}
+      defaultDate={defaultDate}
+      views={views}
+      scrollToTime={defaultDate}
+      popup
+      onSelectEvent={onSelectEvent}
+    />
+  </div>
+  );
+}
