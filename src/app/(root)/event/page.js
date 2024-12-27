b117d18164203ba87f375eec23428da44ed8c691
Jona0002 <jona0002@student.monash.edu>
2024-12-27 22:41:32 +0800

    Adds events display

--- src/app/(root)/event/page.js
+++ src/app/(root)/event/page.js
@@ -1,29 +1,142 @@
-'use client'
+"use client";
 
-import Link from 'next/link';
-import React from 'react';
+import React, { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import {
+  Tabs,
+  Tab,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  useDisclosure,
+  Image,
+} from "@nextui-org/react";
+import { eventData } from "../../../components/demoData";
+import EventModal from "../../../components/EventModal";
+import moment from "moment";
 
+export default function Page() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [selectedEvent, setSelectedEvent] = useState(null);
 
+  const handleSelectEvent = (event) => {
+    setSelectedEvent(event);
+    onOpen();
+  };
 
-const Header = () => {
+  const getStart = (event) => {
+    return moment(event.start).format("DD/mm/yyyy, h:mm a");
+  };
+
   return (
-    <header className="bg-gray-800 text-white p-4">
-      <nav className="flex justify-between items-center">
-        <h1 className="text-xl font-bold">Events/Activites</h1>
-        <div className="space-x-4">
-          
-          <Link href="/availability">
-            <span>Set Availability</span>
-          </Link>
-
-          <Link href="/CreateEvent">
-            <span>Create Event</span>
-          </Link>
-          
-        </div>
-      </nav>
-    </header>
+    <div className="flex flex-col space-y-4 lg:px-16 sm:px-8 px-4 py-4">
+      <h1 className="text-4xl font-bold">Events</h1>
+      <Tabs aria-label="Options" color="primary">
+        <Tab key="scheduling" title="Scheduling">
+          <Card>
+            <CardBody>
+              <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+                {eventData.map((item, index) => (
+                  <Card
+                    key={index}
+                    isPressable
+                    shadow="sm"
+                    onPress={() => handleSelectEvent(item)}
+                  >
+                    <CardBody className="overflow-visible p-0">
+                      <Image
+                        alt={item.title}
+                        className="w-full object-cover h-[140px]"
+                        radius="lg"
+                        shadow="sm"
+                        src="https://raw.githubusercontent.com/RyanHiiMalaysia/event-manager/refs/heads/user-page/public/blue.svg"
+                        width="100%"
+                      />
+                    </CardBody>
+                    <CardFooter className="text-small justify-between">
+                      <b>{item.title}</b>
+                    </CardFooter>
+                  </Card>
+                ))}
+              </div>
+            </CardBody>
+          </Card>
+        </Tab>
+        <Tab key="allocated" title="Allocated">
+          <Card>
+            <CardBody>
+              <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+                {eventData.map((item, index) => (
+                  <Card
+                    key={index}
+                    isPressable
+                    shadow="sm"
+                    onPress={() => handleSelectEvent(item)}
+                  >
+                    <CardBody className="overflow-visible p-0">
+                      <Image
+                        alt={item.title}
+                        className="w-full object-cover h-[140px]"
+                        radius="lg"
+                        shadow="sm"
+                        src="https://raw.githubusercontent.com/RyanHiiMalaysia/event-manager/refs/heads/user-page/public/green.svg"
+                        width="100%"
+                      />
+                    </CardBody>
+                    <CardFooter className="text-small justify-between">
+                      <b>{item.title}</b>
+                      <p className="text-default-500">
+                        <span className="block sm:inline">
+                          {moment(item.start).format("DD/MM/YYYY ")}
+                        </span>
+                        <span className="block sm:inline">
+                          {moment(item.start).format("h:mm a")}
+                        </span>
+                      </p>
+                    </CardFooter>
+                  </Card>
+                ))}
+              </div>
+            </CardBody>
+          </Card>
+        </Tab>
+        <Tab key="organising" title="Organising">
+          <Card>
+            <CardBody>
+              <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+                {eventData.map((item, index) => (
+                  <Card
+                    key={index}
+                    isPressable
+                    shadow="sm"
+                    onPress={() => handleSelectEvent(item)}
+                  >
+                    <CardBody className="overflow-visible p-0">
+                      <Image
+                        alt={item.title}
+                        className="w-full object-cover h-[140px]"
+                        radius="lg"
+                        shadow="sm"
+                        src="https://raw.githubusercontent.com/RyanHiiMalaysia/event-manager/refs/heads/user-page/public/yellow.svg"
+                        width="100%"
+                      />
+                    </CardBody>
+                    <CardFooter className="text-small justify-between">
+                      <b>{item.title}</b>
+                    </CardFooter>
+                  </Card>
+                ))}
+              </div>
+            </CardBody>
+          </Card>
+        </Tab>
+      </Tabs>
+      <EventModal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        selectedEvent={selectedEvent}
+      />
+    </div>
   );
-};
-
-export default Header;
+}
