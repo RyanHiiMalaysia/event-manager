ddb62b0476f19ae3ae4f636c887948e5d09ae65c
Darryl <148646350+darrylteh@users.noreply.github.com>
2024-12-28 14:43:27 +0800

    Move sign up button to home page and add icon to login button

--- src/app/components/NavbarClient.js
+++ src/app/components/NavbarClient.js
@@ -20,6 +20,34 @@ import {
 import { signIn, signOut } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 
+export const UserIcon = ({fill = "currentColor", size, height, width, ...props}) => {
+  return (
+    <svg
+      data-name="Iconly/Curved/Profile"
+      height={size || height || 24}
+      viewBox="0 0 24 24"
+      width={size || width || 24}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke={fill}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        strokeWidth={1.5}
+      >
+        <path
+          d="M11.845 21.662C8.153 21.662 5 21.088 5 18.787s3.133-4.425 6.845-4.425c3.692 0 6.845 2.1 6.845 4.4s-3.134 2.9-6.845 2.9z"
+          data-name="Stroke 1"
+        />
+        <path d="M11.837 11.174a4.372 4.372 0 10-.031 0z" data-name="Stroke 3" />
+      </g>
+    </svg>
+  );
+};
+
 const NavbarClient = ({ session }) => {
   const currentPath = usePathname();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -92,18 +120,13 @@ const NavbarClient = ({ session }) => {
         ) : (
           <>
             <NavbarItem>
-              <Button
+              <Button startContent={<UserIcon />}
                 as="button"
                 color="primary"
                 variant="flat"
                 onPress={() => signIn('google')}
               >
                 Login
-              </Button>
-            </NavbarItem>
-            <NavbarItem>
-              <Button as={Link} href="/signUp" color="primary" variant="flat">
-                Sign Up
               </Button>
             </NavbarItem>
           </>
