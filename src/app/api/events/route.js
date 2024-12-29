e8701990105908c2c791d01dae1d3121633cb0b2
Jona0002 <jona0002@student.monash.edu>
2024-12-30 03:32:37 +0800

    Updates SQL query to only get one event

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -1,17 +1,18 @@
 import { NextResponse } from "next/server";
 import { neon } from "@neondatabase/serverless";
 
-
-// async function fetchEventsDetails(link) {
-//   const sql = neon(`${process.env.DATABASE_URL}`);
-//   return await sql('SELECT * FROM events WHERE event_link = $1', [link]);
-// }
-
 async function fetchEventsDetails() {
   const sql = neon(`${process.env.DATABASE_URL}`);
-  return await sql('SELECT * FROM events');
+  return await sql("SELECT * FROM events");
 }
+
+async function fetchEventDetails(link) {
+  const sql = neon(`${process.env.DATABASE_URL}`);
+  return await sql("SELECT * FROM events WHERE event_link = $1", [link]);
+}
+
 export async function POST(req) {
+  const sql = neon(`${process.env.DATABASE_URL}`);
   try {
     const {
       title,
@@ -24,47 +25,37 @@ export async function POST(req) {
       maxParticipants,
       description,
       link,
-      registrationDeadline,
-      ownerId
+      deadline,
+      ownerId,
     } = await req.json();
-
-    // Connect to the Neon database
-    const sql = neon(`${process.env.DATABASE_URL}`);
 
     await sql`
         INSERT INTO events (
-            event_name, event_duration, event_max_participants, event_description, event_location,
-            event_openingHour, event_closingHour, event_schedule_range_start, event_schedule_range_end, 
-            event_link, event_deadline, event_owner
+            event_title, event_duration, event_schedule_start, event_schedule_end, event_deadline, 
+            event_max_participants, event_location, event_opening_hour, event_closing_hour, event_description, 
+            event_link, event_creator
         )
         VALUES (
-            ${title}, ${duration}, ${maxParticipants}, ${description}, ${location},
-            ${startTime}, ${endTime}, ${startDate}, ${endDate}, ${link}, ${registrationDeadline},
-            ${ownerId}
+            ${title}, ${duration}, ${startDate}, ${endDate}, ${deadline}, ${maxParticipants}, ${location}, ${startTime},
+             ${endTime}, ${description}, ${link}, ${ownerId}
         )
         `;
 
     // Respond with success
-    return NextResponse.json(
-      { message: "Event created successfully."},
-      { status: 200 }
-    );
+    return NextResponse.json({ message: "Event created successfully." }, { status: 200 });
   } catch (error) {
     console.log(error);
-    return NextResponse.json(
-      { message: "Internal Server Error" },
-      { status: 500 }
-    );
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
   }
 }
 
 export async function GET(req) {
   try {
-    const events = await fetchEventsDetails();
-    
-    return new Response(JSON.stringify({ events }), { status: 200 });
+    const url = new URL(req.url);
+    const link = url.searchParams.get("link");
+    const eventData = await fetchEventDetails(link);
+    return new Response(JSON.stringify({ eventData }), { status: 200 });
   } catch (error) {
-    
-    return new Response(JSON.stringify({ message: 'Failed to fetch events' }), { status: 500 });
+    return new Response(JSON.stringify({ message: "Failed to fetch events" }), { status: 500 });
   }
 }
