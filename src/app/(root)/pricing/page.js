ef98338c1b075c16682b375f2e4363b2aff73577
Jona0002 <jona0002@student.monash.edu>
2024-12-29 23:22:15 +0800

    Improves styling for pricing page

--- src/app/(root)/pricing/page.js
+++ src/app/(root)/pricing/page.js
@@ -1,28 +1,22 @@
 "use client";
 
-import React from 'react';
-import { Card, CardBody, CardHeader, Image } from '@nextui-org/react';
+import React from "react";
+import { Card, CardBody, CardFooter, CardHeader, Image } from "@nextui-org/react";
 
 const PricingPage = () => {
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <Card>
-                <CardHeader>
-                    <h2 style={{ fontSize: '32px' }}>Pricing</h2>
-                </CardHeader>
-                <CardBody>
-                    <p style={{ fontSize: '18px' }}>
-                        Get lifetime access to our Event Manager for a one-time purchase of RM5.
-                    </p>
-                    <Image
-                        alt="TNG eWallet QR"
-                        src="tng.jpg"
-                        width={300}
-                    />
-                </CardBody>
-            </Card>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center p-4">
+      <Card className="w-full max-w-md">
+        <CardHeader className="flex mt-4 mx-4">
+          <h2 className="text-3xl bold">Pricing</h2>
+        </CardHeader>
+        <CardBody className="flex flex-col items-center">
+          <p className="text-lg mx-4">Get lifetime access to our Event Manager for a one-time purchase of RM5.</p>
+          <Image alt="TNG eWallet QR" src="tng.jpg" />
+        </CardBody>
+      </Card>
+    </div>
+  );
 };
 
 export default PricingPage;
