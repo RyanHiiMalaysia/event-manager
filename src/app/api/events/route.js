4274f7078b8eb79cb93469089aa871095d2f2899
Jona0002 <jona0002@student.monash.edu>
2024-12-30 15:24:42 +0800

    Adds creator to the userevent table

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -1,11 +1,6 @@
 import { NextResponse } from "next/server";
 import { neon } from "@neondatabase/serverless";
 
-async function fetchEventsDetails() {
-  const sql = neon(`${process.env.DATABASE_URL}`);
-  return await sql("SELECT * FROM events");
-}
-
 async function fetchEventDetails(link) {
   const sql = neon(`${process.env.DATABASE_URL}`);
   return await sql("SELECT * FROM events WHERE event_link = $1", [link]);
@@ -26,25 +21,36 @@ export async function POST(req) {
       description,
       link,
       deadline,
-      ownerId,
+      creator,
     } = await req.json();
 
-    await sql`
-        INSERT INTO events (
-            event_title, event_duration, event_schedule_start, event_schedule_end, event_deadline, 
-            event_max_participants, event_location, event_opening_hour, event_closing_hour, event_description, 
-            event_link, event_creator
-        )
-        VALUES (
-            ${title}, ${duration}, ${startDate}, ${endDate}, ${deadline}, ${maxParticipants}, ${location}, ${startTime},
-             ${endTime}, ${description}, ${link}, ${ownerId}
-        )
-        `;
+    const [insertedEvent] = await sql`
+      INSERT INTO events (
+        event_title, event_duration, event_schedule_start, event_schedule_end, event_deadline, 
+        event_max_participants, event_location, event_opening_hour, event_closing_hour, event_description, 
+        event_link, event_creator
+      ) VALUES (
+        ${title}, ${duration}, ${startDate}, ${endDate}, ${deadline}, ${maxParticipants}, ${location}, ${startTime},
+        ${endTime}, ${description}, ${link}, ${creator}
+      ) RETURNING event_id
+    `;
 
-    // Respond with success
+    const eventID = insertedEvent.event_id;
+
+    await sql`
+        INSERT INTO userevent (
+          ue_user_id, ue_event_id, ue_is_admin
+        ) VALUES (
+          ${creator}, ${eventID}, true
+        )
+      `;
+
+    await sql`COMMIT`;
+
     return NextResponse.json({ message: "Event created successfully." }, { status: 200 });
   } catch (error) {
     console.log(error);
+    await sql`ROLLBACK`;
     return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
   }
 }
