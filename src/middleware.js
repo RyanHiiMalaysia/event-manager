34750cf4d2b9ef11356b70033ce493df9e7c5b38
Darryl <148646350+darrylteh@users.noreply.github.com>
2024-12-30 22:04:15 +0800

    Temporary authentication middleware to check user payment status and redirect accordingly

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-
-
+import { neon } from '@neondatabase/serverless';
 
 export async function middleware(req) {
   const secret = process.env.AUTH_SECRET;
@@ -9,10 +8,27 @@ export async function middleware(req) {
     req: req,
     secret: secret,
     secureCookie: process.env.NODE_ENV === 'production' ? true : false 
-});
+  });
 
   if (!token) {
     // Redirect to pricing page if user is not authenticated
+    return NextResponse.redirect(new URL('/pricing', req.url));
+  }
+
+  const sql = neon(`${process.env.DATABASE_URL}`);
+  const result = await sql('SELECT user_has_paid FROM users WHERE user_email = $1', [token.email]);
+
+  if (result.length === 0) {
+    // Redirect to pricing page if user is not found in the database
+    return NextResponse.redirect(new URL('/pricing', req.url));
+  }
+
+  const userHasPaid = result[0].user_has_paid;
+
+  const pathname = req.nextUrl.pathname;
+
+  if (!userHasPaid && (pathname.startsWith('/calendar') || pathname.startsWith('/event'))) {
+    // Redirect to pricing page if user has not paid and is trying to access calendar or event pages
     return NextResponse.redirect(new URL('/pricing', req.url));
   }
 
