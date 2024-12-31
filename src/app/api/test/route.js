2f8c6300c999fb2122264599922453520340bb5a
Darryl <148646350+darrylteh@users.noreply.github.com>
2024-12-31 23:05:22 +0800

    Add cron job API endpoint and configuration

---
+++ src/app/api/test/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+    const authHeader = request.headers.get('authorization');
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+        return new Response('Unauthorized', {
+            status: 401,
+        });
+    }
+
+    console.log("Cron Job Ran at ", new Date());
+
+    return NextResponse.json({
+        message: "Cron Job Ran at " + new Date()
+    });
+}
