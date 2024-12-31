441144e30f9d60f0ded9dfa502e0471b1d6b5f3e
Darryl <148646350+darrylteh@users.noreply.github.com>
2024-12-31 17:45:25 +0800

    Add FAQ component to home page

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/hero";
 import Pricing from "@/components/pricing";
+import Faq from "@/components/faq";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-dvh">
       <Hero />
       <Pricing />
+      <Faq />
     </main>
   );
 }
