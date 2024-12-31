234e80c7c2720e1c35956f3242bb7d3fe613037b
Jona0002 <jona0002@student.monash.edu>
2024-12-31 18:25:04 +0800

    Fixes get request with search params and id getter methods and adds deletion to post request

--- src/app/api/user-event/schedule/route.js
+++ src/app/api/user-event/schedule/route.js
@@ -7,24 +7,18 @@ function getDatabaseConnection() {
 }
 
 async function getUserID(sql, email) {
-  const [user_id] = await sql`
-        SELECT user_id FROM users WHERE user_email = ${email}
-    `;
-  return user_id;
+  const [userData] = await sql`SELECT user_id FROM users WHERE user_email = ${email}`;
+  return userData.user_id;
 }
 
 async function getEventID(sql, link) {
-  const [event_id] = await sql`
-        SELECT event_id FROM events WHERE event_link = ${link}
-    `;
-  return event_id;
+  const [eventData] = await sql`SELECT event_id FROM events WHERE event_link = ${link}`;
+  return eventData.event_id;
 }
 
 async function getUsereventID(sql, user_id, event_id) {
-  const [userevent_id] = await sql`
-        SELECT userevent_id FROM userevent WHERE user_id = ${user_id} AND event_id = ${event_id}
-    `;
-  return userevent_id;
+  const [usereventData] = await sql`SELECT ue_id FROM userevent WHERE user_id = ${user_id} AND event_id = ${event_id}`;
+  return usereventData.ue_id;
 }
 
 // Function to fetch the freetimes of a user for a specific event
@@ -32,10 +26,11 @@ async function fetchFreetimes(user_email, event_link) {
   const sql = getDatabaseConnection();
   const user_id = await getUserID(sql, user_email);
   const event_id = await getEventID(sql, event_link);
+  console.log(user_id, event_id);
   const query = sql`
     SELECT
-        ft_start,
-        ft_end,
+        ft_start as start,
+        ft_end as end
     FROM 
         freetimes NATURAL JOIN userevent
     WHERE
@@ -48,21 +43,27 @@ export async function POST(req) {
   const sql = getDatabaseConnection();
   try {
     const { user_email, event_link, freetimes } = await req.json();
-    const [user_id, event_id] = await Promise.all([getUserID(sql, user_email), getEventID(sql, event_link)]);
+    const user_id = await getUserID(sql, user_email);
+    const event_id = await getEventID(sql, event_link);
     const ue_id = await getUsereventID(sql, user_id, event_id);
 
+    await sql `BEGIN`
+    await sql`
+      DELETE FROM freetimes
+      WHERE ue_id = ${ue_id}
+    `;
+
     freetimes.forEach(async (freetime) => {
-      await sql`BEGIN`;
       await sql`
         INSERT INTO freetimes (
           ue_id, ft_start, ft_end
         )
         VALUES (${ue_id}, ${freetime.start}, ${freetime.end})
       `;
-      await sql`COMMIT`;
     });
+    await sql`COMMIT`;
 
-    return NextResponse.json({ message: "Free times added successfully." }, { status: 200 });
+    return NextResponse.json({ message: "Free times updated successfully" }, { status: 200 });
   } catch (error) {
     console.log(error);
     await sql`ROLLBACK`;
@@ -72,8 +73,10 @@ export async function POST(req) {
 
 export async function GET(req) {
   try {
-    const { user_email, event_link } = await req.json();
-    const freeTimes = await fetchFreetimes(user_email, event_link);
+    const url = new URL(req.url);
+    const email = url.searchParams.get("email");
+    const link = url.searchParams.get("link");
+    const freeTimes = await fetchFreetimes(email, link);
     return new Response(JSON.stringify({ freeTimes }), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify({ message: "Failed to fetch events" }), { status: 500 });
