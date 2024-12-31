0be62ad3bcc3a26ac4c598561974f70bc964d624
Darryl <148646350+darrylteh@users.noreply.github.com>
2024-12-31 19:17:36 +0800

    Refactor Profile component to use NextUI Card and Avatar

--- src/app/(root)/profile/page.js
+++ src/app/(root)/profile/page.js
@@ -2,7 +2,9 @@
 
 import React, { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
-import {Spinner} from "@nextui-org/spinner";
+import { Spinner } from "@nextui-org/react";
+import { Card, CardBody, CardHeader, CardFooter } from "@nextui-org/react";
+import { Avatar } from "@nextui-org/react";
 
 const Profile = () => {
   const { data: session, status } = useSession();
@@ -50,13 +52,16 @@ const Profile = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold text-center text-gray-900">Profile</h2>
-        <div className="space-y-4">
-          <div>
-            <label className="block text-sm font-medium text-gray-700">User ID</label>
-            <p className="mt-1 text-sm text-gray-900">{user.user_id}</p>
-          </div>
+      <Card className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+        <CardHeader className="flex flex-col items-center">
+          <Avatar referrerPolicy={'no-referrer'}
+                            size="xl"
+                            src={session.user.image}
+                            showFallback
+                          />
+          <h2 className="text-2xl font-bold text-center text-gray-900">Profile</h2>
+        </CardHeader>
+        <CardBody className="space-y-4">
           <div>
             <label className="block text-sm font-medium text-gray-700">Name</label>
             <p className="mt-1 text-sm text-gray-900">{user.user_name}</p>
@@ -69,8 +74,10 @@ const Profile = () => {
             <label className="block text-sm font-medium text-gray-700">Has Paid?</label>
             <p className="mt-1 text-sm text-gray-900">{user.user_has_paid ? 'Yes' : 'No'}</p>
           </div>
-        </div>
-      </div>
+        </CardBody>
+        <CardFooter className="flex justify-center">
+        </CardFooter>
+      </Card>
     </div>
   );
 };
