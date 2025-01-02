828c268777e1fb451bde505ee1acf65ce7799c09
Darryl <148646350+darrylteh@users.noreply.github.com>
2025-01-02 15:27:57 +0800

    Add user payment status check in Pricing component to conditionally render button and animation

--- src/components/pricing.jsx
+++ src/components/pricing.jsx
@@ -4,8 +4,10 @@ import { Button } from "@nextui-org/button";
 import { Divider } from "@nextui-org/divider";
 import { motion } from "framer-motion";
 import { Link } from "@nextui-org/link";
+import { useSession } from "next-auth/react";
 
 export default function Pricing() {
+    const { data: session, status } = useSession();
     const plans = [
         {
             name: "One-Time Purchase",
@@ -41,54 +43,63 @@ export default function Pricing() {
             </div>
             <div className="mt-16 gap-10 grid lg:grid-cols-1 place-content-center">
                 {plans.map((item, idx) => (
-                    <Card
-                        key={idx}
-                        shadow="none"
-                        className={`relative rounded-[20px] p-[2px] will-change-transform ${item.isMostPop ? "sm:scale-110" : ""
-                            }`}
-                    >
-                        {item.isMostPop ? (
-                            <span className="absolute inset-[-1000%] animate-[spin_6s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#016FEE_70%,#C7DBFB_100%)]" />
-                        ) : (
-                            <span className="absolute inset-[-1000%] bg-border" />
-                        )}
-                        <div className="z-[2] flex flex-col justify-between w-full h-full bg-card rounded-[18px] p-5 opacity-75">
-                            <CardBody className="w-full flex items-start gap-3">
-                                <div className="flex flex-col">
-                                    <h4 className="text-xl font-light">{item.name}</h4>
-                                    <span className="text-muted-foreground text-sm font-light">
-                                        {item.desc}
-                                    </span>
-                                </div>
-                                <span className="text-2xl font-light">RM{item.price}</span>
+                    <div key={idx} className="relative flex flex-col items-center">
+                        <Card
+                            shadow="none"
+                            className={`relative rounded-[20px] p-[2px] will-change-transform ${item.isMostPop ? "sm:scale-110" : ""
+                                }`}
+                        >
+                            {item.isMostPop ? (
+                                <span className="absolute inset-[-1000%] animate-[spin_6s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#016FEE_70%,#C7DBFB_100%)]" />
+                            ) : (
+                                <span className="absolute inset-[-1000%] bg-border" />
+                            )}
+                            <div className="z-[2] flex flex-col justify-between w-full h-full bg-card rounded-[18px] p-5 opacity-75">
+                                <CardBody className="w-full flex items-start gap-3">
+                                    <div className="flex flex-col">
+                                        <h4 className="text-xl font-light">{item.name}</h4>
+                                        <span className="text-muted-foreground text-sm font-light">
+                                            {item.desc}
+                                        </span>
+                                    </div>
+                                    <span className="text-2xl font-light">RM{item.price}</span>
 
-                                <Divider />
+                                    <Divider />
 
-                                <div className="flex flex-col gap-5 pb-5">
-                                    <span className="text-muted-foreground text-sm font-light">
-                                        Includes
-                                    </span>
-                                    <ul className="flex flex-col gap-2">
-                                        {item.features.map((feature, index) => (
-                                            <li key={index} className="text-sm font-light">
-                                                {feature}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            </CardBody>
-                            <CardFooter className="p-0">
-                                <Button
-                                    className="w-full"
-                                    variant="solid"
-                                    color={item.isMostPop ? "primary" : "default"}
-                                    as={Link} href="/pricing"
-                                >
-                                    Get Started
-                                </Button>
-                            </CardFooter>
-                        </div>
-                    </Card>
+                                    <div className="flex flex-col gap-5 pb-5">
+                                        <span className="text-muted-foreground text-sm font-light">
+                                            Includes
+                                        </span>
+                                        <ul className="flex flex-col gap-2">
+                                            {item.features.map((feature, index) => (
+                                                <li key={index} className="text-sm font-light">
+                                                    {feature}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </CardBody>
+                                <CardFooter className="p-0 flex items-center justify-center">
+                                    {session?.user?.user_has_paid && (
+                                        <img src="dancingBear.gif" alt="Dancing Bear" className="h-16 mr-4" draggable="false"/>
+                                    )}
+                                    <Button
+                                        className="w-full"
+                                        variant="solid"
+                                        color={item.isMostPop ? "primary" : "default"}
+                                        as={Link}
+                                        href="/pricing"
+                                        isDisabled={session?.user?.user_has_paid}
+                                    >
+                                        {session?.user?.user_has_paid ? "Thank you!" : "Get Started"}
+                                    </Button>
+                                    {session?.user?.user_has_paid && (
+                                        <img src="dancingBear.gif" alt="Dancing Bear" className="h-16 ml-4" draggable="false"/>
+                                    )}
+                                </CardFooter>
+                            </div>
+                        </Card>
+                    </div>
                 ))}
             </div>
         </motion.section>
