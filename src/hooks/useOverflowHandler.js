3827f687cf1327311ee9bd0c28ba9ed10ff1f4f2
Jona0002 <jona0002@student.monash.edu>
2025-01-03 00:32:17 +0800

    Scrolls back up when scrolled down and switching from a different page

--- src/hooks/useOverflowHandler.js
+++ src/hooks/useOverflowHandler.js
@@ -17,6 +17,7 @@ export default function useOverflowHandler(size) {
         }
       }
     };
+    window.scrollTo(0, 0);
 
     handleResize();
     window.addEventListener("resize", handleResize);
