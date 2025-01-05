6692802651bffb59edaf804a803dcbdafa954da3
Jona0002 <jona0002@student.monash.edu>
2025-01-05 18:55:00 +0800

    Adds admin check in verifyParticipation function to check if the user is admin with a get request

--- src/app/api/user-event/route.js
+++ src/app/api/user-event/route.js
@@ -10,14 +10,18 @@ function getDatabaseConnection() {
 async function fetchUserEvents(user_email, hasAllocated, isAdmin, isPast) {
   const sql = getDatabaseConnection();
   const boolToQuery = (bool, trueQuery, falseQuery) => (bool === null ? "" : bool ? trueQuery : falseQuery);
-  
+
   const allocatedQuery = boolToQuery(
     hasAllocated,
     "AND event_allocated_start IS NOT NULL",
     "AND event_allocated_start IS NULL"
   );
   const adminQuery = boolToQuery(isAdmin, "AND ue_is_admin = TRUE", "AND ue_is_admin = FALSE");
-  const pastQuery = boolToQuery(isPast, "AND event_allocated_end < NOW()", "AND (event_allocated_end >= NOW() OR event_allocated_end IS NULL)");
+  const pastQuery = boolToQuery(
+    isPast,
+    "AND event_allocated_end < NOW()",
+    "AND (event_allocated_end >= NOW() OR event_allocated_end IS NULL)"
+  );
 
   let query = `
   SELECT 
@@ -48,22 +52,26 @@ async function fetchUserEvents(user_email, hasAllocated, isAdmin, isPast) {
 }
 
 // Function to verify if user is already in event
-async function verifyParticipation(sql, user_email, event_link) {
-  const [user_id] = await sql`
+async function verifyParticipation(sql, user_email, event_link, isAdmin = null) {
+  const boolToQuery = (bool, trueQuery, falseQuery) => (bool === null ? "" : bool ? trueQuery : falseQuery);
+  const adminQuery = boolToQuery(isAdmin, "AND ue_is_admin = TRUE", "AND ue_is_admin = FALSE");
+  const query = `
     SELECT
       user_id
     FROM
       users NATURAL JOIN userevent
     WHERE
-      user_email = ${user_email}
+      user_email = $1
     AND
-      event_id = (SELECT event_id FROM events WHERE event_link = ${event_link})
+      event_id = (SELECT event_id FROM events WHERE event_link = $2)
+    ${adminQuery}
   `;
+
+  const [user_id] = await sql(query, [user_email, event_link]);
   return user_id ? true : false;
 }
-//Function to check the event has reached maximum participants 
+//Function to check the event has reached maximum participants
 async function findNumberOfParticipants(sql, event_link) {
-
   const currentNumberOfParticipants = await sql`
     SELECT
       COUNT(user_id)
@@ -72,7 +80,7 @@ async function findNumberOfParticipants(sql, event_link) {
     WHERE
       event_id = (SELECT event_id FROM events WHERE event_link = ${event_link})
   `;
-  
+
   return currentNumberOfParticipants;
 }
 
@@ -103,17 +111,17 @@ export async function GET(req) {
     const isAdmin = strToBool(url.searchParams.get("isAdmin"));
     const isPast = strToBool(url.searchParams.get("isPast"));
     const numberOfParticipants = strToBool(url.searchParams.get("findNumberOfParticipants"));
-    const link = url.searchParams.get("link")
+    const link = url.searchParams.get("link");
     const findIsUserIn = strToBool(url.searchParams.get("findIsUserIn"));
-  
-    if(numberOfParticipants){
+
+    if (numberOfParticipants) {
       const result = await findNumberOfParticipants(sql, link);
 
-      return new Response(JSON.stringify({ result:result }), { status: 200 });
+      return new Response(JSON.stringify({ result: result }), { status: 200 });
     }
-    if(findIsUserIn){
-      const isUserIn = await verifyParticipation(sql, email, link);
-      return new Response(JSON.stringify({ result: isUserIn }), { status: 200 })
+    if (findIsUserIn) {
+      const isUserIn = await verifyParticipation(sql, email, link, isAdmin);
+      return new Response(JSON.stringify({ result: isUserIn }), { status: 200 });
     }
     const eventData = await fetchUserEvents(email, hasAllocated, isAdmin, isPast);
     return new Response(JSON.stringify({ eventData }), { status: 200 });
