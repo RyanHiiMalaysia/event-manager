09107489f7484cf56c921189801109d30746040f
YuTEMs <138864422+YuTEMs@users.noreply.github.com>
2025-01-08 10:58:24 +0800

    Modified POST request

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -3,13 +3,17 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(req) {
   try {
+    // authenthication
+    
+    const { user_email, userFirstName } = await req.json();
+    
     const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
+      from: 'Acme <onboarding@resend.dev>', // 'Acme <noreply@allocato.net>'
+      to: [user_email],
       subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
+      react: EmailTemplate(userFirstName),
     });
 
     if (error) {
