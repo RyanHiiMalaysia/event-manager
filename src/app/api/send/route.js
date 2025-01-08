dbf159ce884be51bd22cfb65d2fc31649cfe2921
YuTEMs <138864422+YuTEMs@users.noreply.github.com>
2025-01-08 17:17:09 +0800

    modularized api to use layout not from imports anymore

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -7,23 +7,26 @@ export async function POST(req) {
   try {
     // Authentication (if needed)
 
-    const { user_email, userFirstName, subject } = await req.json();
+    const { user_email, layout, subject } = await req.json();
+    //const htmlContent = ReactDOMServer.renderToString(layout);
+    
 
     const { data, error } = await resend.emails.send({
-      from: 'Do not reply to this email (noreply@allocato.net) <noreply@allocato.net>', // 'Acme <noreply@allocato.net>'
+      from: 'Do not reply to this email <noreply@allocato.net>', // 'Acme <noreply@allocato.net>'
       to: [user_email],
       subject: subject,
-      react: EmailTemplate(userFirstName),
+      html: layout,
     });
     
     if (error) {
-      console.log("this one")
+
       return new Response(JSON.stringify({ error }), { status: 500 });
     }
 
     return new Response(JSON.stringify(data), { status: 200 });
   } catch (error) {
-    //console.error('Error sending email:', error);
+    //console.log(layout)
+    console.log(htmlContent);
     return new Response(JSON.stringify({ error: error.message }), { status: 500 });
   }
 }
