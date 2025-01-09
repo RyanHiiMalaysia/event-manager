9faa0ba2c74d1c87bf5cba030bebdb7c20ee7f05
Darryl <148646350+darrylteh@users.noreply.github.com>
2025-01-09 22:42:54 +0800

    Improve layout and clarity of payment instructions on PricingPage

--- src/app/(root)/pricing/PricingPage.js
+++ src/app/(root)/pricing/PricingPage.js
@@ -12,12 +12,27 @@ export default function PricingPage() {
         <CardBody className="flex flex-col items-center p-6">
           <p className="text-lg text-center mb-4">Create unlimited events with Allocato's Pro plan, for a one-time purchase of RM5.</p>
           <Image alt="TNG eWallet QR" src="tng.jpg" />
-          <ol className="list-decimal text-lg mx-4 mt-1 space-y-2">
-            <li>If you haven't already, <Link href="/signup" className="text-lg">sign up</Link> with your email.</li>
-            <li>Scan the QR code above.</li>
-            <li>Make a payment of RM5.</li>
-            <li>Message 016-3308777 on Whatsapp with proof of payment (screenshot of payment) along with your email address.</li>
-            <li>Your account will be registered with the Pro plan within 12 hours.</li>
+          <ol className="list-decimal text-lg mx-4 mt-1 space-y-4">
+            <li className="flex items-start">
+              <span className="mr-2">1.</span>
+              <span>If you haven't already, <Link href="/signup" className="text-lg">sign up</Link> with your email.</span>
+            </li>
+            <li className="flex items-start">
+              <span className="mr-2">2.</span>
+              <span>Scan the QR code above.</span>
+            </li>
+            <li className="flex items-start">
+              <span className="mr-2">3.</span>
+              <span>Make a payment of RM5.</span>
+            </li>
+            <li className="flex items-start">
+              <span className="mr-2">4.</span>
+              <span>Message 016-3308777 on Whatsapp with proof of payment (screenshot of payment) along with your email address.</span>
+            </li>
+            <li className="flex items-start">
+              <span className="mr-2">5.</span>
+              <span>Your account will be registered with the Pro plan within 12 hours with email confirmation.</span>
+            </li>
           </ol>
         </CardBody>
       </Card>
