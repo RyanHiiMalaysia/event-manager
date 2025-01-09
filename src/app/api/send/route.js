c584408fc89a7d7b0cb55a2902397b685a6ff514
YuTEMs <138864422+YuTEMs@users.noreply.github.com>
2025-01-09 20:35:56 +0800

    New layout for event notifications, using switch cases

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,32 +1,36 @@
-
 import { Resend } from 'resend';
+import { CreateEvent } from '@/components/email/CreateEvent';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
+  
   try {
     // Authentication (if needed)
 
-    const { user_email, layout, subject } = await req.json();
-    //const htmlContent = ReactDOMServer.renderToString(layout);
+    const { user_email, layout_choice, subject, userName, event_link } = await req.json();
     
+    let layout;
+    switch (layout_choice) {
+      case 'CreateEvent':
+        layout = CreateEvent(userName, event_link);
+        break;
+      default:
+        console.log('This should not be printed out')
+        break;
+    }
+    console.log('Layout:', layout);
 
     const { data, error } = await resend.emails.send({
       from: 'Do not reply to this email <noreply@allocato.net>', // 'Acme <noreply@allocato.net>'
       to: [user_email],
       subject: subject,
-      html: layout,
+      react: layout,
     });
     
-    if (error) {
-
-      return new Response(JSON.stringify({ error }), { status: 500 });
-    }
 
     return new Response(JSON.stringify(data), { status: 200 });
   } catch (error) {
-    //console.log(layout)
-    console.log(htmlContent);
     return new Response(JSON.stringify({ error: error.message }), { status: 500 });
   }
 }
---
+++ src/components/email/CreateEvent.jsx
@@ -0,0 +1,6 @@
+export const CreateEvent = ( firstName, eventLink ) => (
+    <div>
+      <h1>You have successfully created an event, {firstName}!</h1>
+      <p>Here is your event link: {eventLink}</p>
+    </div>
+  );
