c75a8a254577f3c53e6311c56d0f09ae6264e2c3
Jona0002 <jona0002@student.monash.edu>
2025-01-11 00:03:59 +0800

    Adds action cards on the admin page for admin features and adds settings to change forceAdmin and allocated time

--- src/app/(root)/event/[uniqueLink]/admin/page.js
+++ src/app/(root)/event/[uniqueLink]/admin/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useSession } from "next-auth/react";
-import { useState, useEffect, forwardRef } from "react";
+import { useState, useEffect } from "react";
 import ActionCard from "@/components/ActionCard";
 import Error from "next/error";
 import { useRouter } from "next/navigation";
@@ -13,7 +13,9 @@ export default function Page() {
   const [isAdmin, setIsAdmin] = useState(false);
   const [dataFetched, setDataFetched] = useState(false);
   const [error, setError] = useState(null);
-  
+  const [forceAdmin, setForceAdmin] = useState(false);
+  const [allocatedStart, setAllocatedStart] = useState(null);
+  const [allocateTimes, setAllocateTimes] = useState([]);
   const router = useRouter();
 
   useEffect(() => {
@@ -28,20 +30,48 @@ export default function Page() {
   useEffect(() => {
     const fetchAdminStatus = async () => {
       try {
-        const response = await fetch(
-          `/api/user-event?findIsUserIn=true&link=${eventLink}&email=${session.user.email}`,
-          {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-          }
-        );
+        const headers = {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        };
 
-        const result = await response.json();
-        if (!response.ok) {
-          setError(result.message);
-          return;
-        }
-        setIsAdmin(result.result);
+        const fetchData = async (link) => {
+          const response = await fetch(link, headers);
+          const result = await response.json();
+          if (!response.ok) {
+            throw new Error(result.message || "Failed to fetch events");
+          }
+          return result;
+        };
+
+        const [forceAdminData, isAdminData, allocateTimesData] = await Promise.all([
+          fetchData(`/api/events?link=${eventLink}&forceAdmin=true`),
+          fetchData(`/api/user-event?findIsUserIn=true&link=${eventLink}&email=${session.user.email}`),
+          fetchData(`/api/allocatetimes?link=${eventLink}`),
+        ]);
+
+        setForceAdmin(forceAdminData.result[0].event_force_admin);
+        setAllocatedStart(forceAdminData.result[0].event_allocated_start);
+        setIsAdmin(isAdminData.result);
+        setAllocateTimes(
+          allocateTimesData.allocateTimes.map((time, index) => {
+            const startDate = new Date(time.at_start);
+            const endDate = new Date(time.at_end);
+            const dateOptions = { day: "2-digit", month: "2-digit", year: "numeric" };
+            const startDateString = startDate.toLocaleDateString("en-GB", dateOptions);
+            const endDateString = endDate.toLocaleDateString("en-GB", dateOptions);
+            const timeOptions = { hour: "2-digit", minute: "2-digit" };
+            const startTimeString = startDate.toLocaleTimeString([], timeOptions);
+            const endTimeString = endDate.toLocaleTimeString([], timeOptions);
+
+            const label =
+              startDateString === endDateString
+                ? `${startDateString} ${startTimeString} - ${endTimeString}`
+                : `${startDateString} ${startTimeString} - ${endDateString} ${endTimeString}`;
+
+            return { ...time, key: index, label };
+          })
+        );
       } catch (error) {
         setError(error);
       } finally {
@@ -54,64 +84,54 @@ export default function Page() {
     }
   }, [session, eventLink, dataFetched]);
 
-  const SetCard = forwardRef(({ title, icon, description, className }, ref) => {
-    return (
-      <Card ref={ref} className={cn("border-small", "border-default-200", className)} shadow="sm">
-        <CardBody className="flex h-full flex-row items-start gap-3 p-4">
-          <div className={cn("item-center flex rounded-medium border p-2", "bg-primary-50 border-primary-100")}>
-            <Icon className="text-primary" icon={icon} width={24} />
-          </div>
-          <div className="flex flex-col">
-            <p className="text-medium">{title}</p>
-            <p className="text-small text-default-400">{description}</p>
-          </div>
-        </CardBody>
-        <Divider />
-        <div className="flex flex-col gap-2">
-          <Switch
-            classNames={{
-              base: cn(
-                "inline-flex flex-row-reverse w-full max-w-md bg-content1 hover:bg-content2 items-center",
-                "justify-between cursor-pointer rounded-lg gap-2 p-4 border-2 border-transparent"
-              )
-            }}
-          >
-            <div className="flex flex-col gap-1">
-              <p className="text-medium">Force Admin</p>
-              <p className="text-tiny text-default-400">Includes all admins in the allocated time</p>
-            </div>
-          </Switch>
-        </div>
-      </Card>
-    );
-  });
+  const updateForceAdmin = (value) => {
+    const update = async (value) => {
+      try {
+        const response = await fetch(`/api/events`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            link: eventLink,
+            edit: true,
+            forceAdmin: value,
+          }),
+        });
 
-  const ChangeTimeCard = forwardRef(({ title, icon, description, className }, ref) => {
-    return (
-      <Card ref={ref} className={cn("border-small", "border-default-200", className)} shadow="sm">
-        <CardBody className="flex h-full flex-row items-start gap-3 p-4">
-          <div className={cn("item-center flex rounded-medium border p-2", "bg-primary-50 border-primary-100")}>
-            <Icon className="text-primary" icon={icon} width={24} />
-          </div>
-          <div className="flex flex-col">
-            <p className="text-medium">{title}</p>
-            <p className="text-small text-default-400">{description}</p>
-          </div>
+        const result = await response.json();
+        if (!response.ok) {
+          setError(result.message);
+          return;
+        }
+      } catch (error) {
+        setError(error);
+      }
+    };
+    setForceAdmin(value);
+    update(value);
+  };
 
-        </CardBody>
-        <Divider />
-          <div className="flex flex-col gap-2">
-            <Select>
-              <SelectItem value="1">1 hour</SelectItem>
-              <SelectItem value="2">2 hours</SelectItem>
-              <SelectItem value="3">3 hours</SelectItem>
-              <SelectItem value="4">4 hours</SelectItem>
-              <SelectItem value="5">5 hours</SelectItem>
-            </Select>
-          </div>
-      </Card>
-    );
-  });
+  const updateAllocatedTime = ({ at_id }) => {
+    const update = async (at_id) => {
+      try {
+        const response = await fetch(`/api/allocatetimes`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            link: eventLink,
+            at_id: at_id,
+          }),
+        });
+        const result = await response.json();
+        if (!response.ok) {
+          setError(result.message);
+          return;
+        }
+      } catch (error) {
+        setError(error);
+      }
+    };
+    update(at_id);
+  };
 
   if (status === "loading" || !dataFetched) {
     return null;
@@ -143,16 +163,84 @@ export default function Page() {
             color="warning"
             onPress={() => router.push(`/event/${eventLink}/participants`)}
           />
-          <SetCard
-            description="Edit the allocation settings for the event."
-            title="Allocation Settings"
-            icon="solar:settings-linear"
-          />
-          <ChangeTimeCard
-            description="Change the time of the event."
-            title="Change Allocated Time"
-            icon="solar:sort-by-time-linear"
-          />
+
+          <Card className={cn("border-small", "border-default-200")} shadow="sm">
+            <CardBody className="flex h-full flex-row items-start gap-3 p-4">
+              <div className={cn("item-center flex rounded-medium border p-2", "bg-primary-50 border-primary-100")}>
+                <Icon className="text-primary" icon="solar:settings-linear" width={24} />
+              </div>
+              <div className="flex flex-col">
+                <p className="text-medium">Allocation Settings</p>
+                <p className="text-small text-default-400">Edit the allocation settings for the event.</p>
+              </div>
+            </CardBody>
+            <Divider />
+            <div className="flex flex-col gap-2">
+              <Switch
+                onValueChange={updateForceAdmin}
+                isSelected={forceAdmin}
+                isDisabled={allocatedStart !== null}
+                classNames={{
+                  base: cn(
+                    "inline-flex flex-row-reverse w-full max-w-md bg-content1 hover:bg-content2 items-center",
+                    "justify-between cursor-pointer rounded-lg gap-2 p-4 border-2 border-transparent"
+                  ),
+                }}
+              >
+                <div className="flex flex-col gap-1">
+                  <p className="text-medium">Force Admin</p>
+                  <p className="text-tiny text-default-400">Includes all admins in the allocated time</p>
+                </div>
+              </Switch>
+            </div>
+          </Card>
+          <Card className={cn("border-small", "border-default-200")} shadow="sm">
+            <CardBody className="flex h-full flex-row items-start gap-3 p-4">
+              <div className={cn("item-center flex rounded-medium border p-2", "bg-secondary-50 border-secondary-100")}>
+                <Icon className="text-secondary" icon="solar:sort-by-time-linear" width={24} />
+              </div>
+              <div className="flex flex-col">
+                <p className="text-medium">Change Allocated Time</p>
+                <p className="text-small text-default-400">Change the time of the event.</p>
+              </div>
+            </CardBody>
+            <Divider />
+            <div className="flex flex-col gap-2">
+              <Select
+                variant="flat"
+                placeholder="Deadline has not been passed"
+                items={allocateTimes}
+                defaultSelectedKeys={[String(allocateTimes.findIndex((time) => time.at_start === allocatedStart))]}
+                disallowEmptySelection
+                disabled={allocatedStart === null}
+                aria-label="Select allocated time"
+                onChange={(selected) => updateAllocatedTime(allocateTimes[selected.target.value])}
+                // renderValue={(items) => {
+                //   return items.map((item) => (
+                //     <SelectItem key={item.key}>
+                //       <div className="flex gap-2 items-center">
+                //         <div className="flex flex-col">
+                //           <span className="text-small">{item.data}</span>
+                //           <span className="text-small text-default-400">Participants: {item.data.at_participants}</span>
+                //         </div>
+                //       </div>
+                //     </SelectItem>
+                //   ));
+                // }}
+              >
+                {allocateTimes.map(({ key, label, at_participants }) => (
+                  <SelectItem key={key} textValue={label}>
+                    <div className="flex gap-2 items-center">
+                      <div className="flex flex-col">
+                        <span className="text-small">{label}</span>
+                        <span className="text-small text-default-400">Participants: {at_participants}</span>
+                      </div>
+                    </div>
+                  </SelectItem>
+                ))}
+              </Select>
+            </div>
+          </Card>
         </div>
       </div>
     );
