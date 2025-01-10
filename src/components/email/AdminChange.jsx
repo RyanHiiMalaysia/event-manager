96d9da36993dde6d20ed5172fa8ca890aa90ebad
Darryl <148646350+darrylteh@users.noreply.github.com>
2025-01-10 18:45:10 +0800

    Refine admin change notification wording for clarity

--- src/components/email/AdminChange.jsx
+++ src/components/email/AdminChange.jsx
@@ -1,7 +1,7 @@
 export const AdminChange = (becomeAdmin, eventName, eventLink) => (
 
   <div>
-    <h1>{becomeAdmin ? 'You have been made an Admin' : 'You have been removed as an Admin'}</h1>
+    <h1>{becomeAdmin ? 'You are now an admin' : 'You are no longer an admin'}</h1>
     <p>You {becomeAdmin ? " have been made an admin of the event: " : " have been removed as an admin from the event: "}<strong>{eventName}</strong>.</p>
     <p>Here is the link : {eventLink}</p>
   </div>
