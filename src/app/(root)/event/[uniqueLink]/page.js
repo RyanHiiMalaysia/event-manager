4eff39f8fde79c730a0a92a55890c2fe8bf6f6fb
Jona0002 <jona0002@student.monash.edu>
2025-01-13 16:01:45 +0800

    Adds custom metadata for event page

--- src/app/(root)/event/[uniqueLink]/page.js
+++ src/app/(root)/event/[uniqueLink]/page.js
@@ -1,8 +1,12 @@
 import EventDetailsPage from "./EventDetailsPage";
 
-export const metadata = {
-  title: "Event Details",
-  description: "Shows the details of the event",
-};
+export async function generateMetadata({ params }) {
+  const apiLink = `https://event-manager-opal.vercel.app/api/events?link=${(await params).uniqueLink}&metadata=true`;
+  const {eventData} = await fetch(apiLink).then((res) => res.json());
+  return {
+    title: eventData[0].title,
+    description: eventData[0].description,
+  };
+}
 
 export default EventDetailsPage;
